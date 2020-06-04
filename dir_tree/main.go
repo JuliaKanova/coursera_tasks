9879374b8fe8216087437f22ccbc808308c0600f
Julia Kanova <iuliia.kanova@gmail.com>
2020-06-04 20:13:15 +0300

    Show file and directories

--- dir_tree/main.go
+++ dir_tree/main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func dirTree(dirname string, withFile bool) {
-	fmt.Println(dirname, withFile)
+	var visitedDir []string
+	err := filepath.Walk(dirname,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !withFile {
+				if info.IsDir() {
+					fmt.Println(path, info.Size())
+					visitedDir = append(visitedDir, path)
+
+				}
+			} else {
+				fmt.Println(path, info.Size())
+				visitedDir = append(visitedDir, path)
+			}
+			return nil
+		})
+	fmt.Println(visitedDir)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func main() {
